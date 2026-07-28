bb6c64e397beafa1f95f86245687df013b16a2cc
JDVegas <durand-vegas.johann@hotmail.fr>
2026-07-28 15:07:51 +0200

    update  to add functions to cleanly open and close database connection

--- 01_Pilier_de_lIA/Formation_SQL/Flashcards_Exercice/database.py
+++ 01_Pilier_de_lIA/Formation_SQL/Flashcards_Exercice/database.py
@@ -3,6 +3,8 @@
 
 # -- SETUP
 # -- x-------------------------------------x --
+# Srandard libraries 
+from typing import Tuple
 
 # Libraries sql
 import sqlite3
@@ -13,24 +15,41 @@ import sqlite3
 
 # FUNCTIONS
 # -- x-------------------------------------x --
-# Function to initialise the database
-def init_db():
-    """Function that create the database tables and populate with a set of default themes """
 
-
-    # CONFIGURATION
-    # -- x----------------x --
+# Function to connect to the database
+def connect_database() -> Tuple:
+    """ Connect database and generate a cursor """ 
     # Create a connexion to the database
     conn = sqlite3.connect("flashcards.db")
+
+    # Activate the foreign_keys parameters 
+    conn.execute("PRAGMA foreign_keys = ON;")
+
+    # Display a message
+    print("\nConnection to the database is operational !\n")
 
     # Create a cursor to execute sql requests
     cursor = conn.cursor()
 
+    return (conn, cursor)
+
+# Function to save and disconnect the database
+def close_db_connection(conn):
+    """ Save the modification done and properly close the database connection """
+    # Validate modifications
+    conn.commit()
+    # Close the connection
+    conn.close()
     # Display a message
-    print("\nConnection to the database is operational !\n")
-    # -- x----------------x --
+    print('\nThe database connexion have been properly closed !\n')
 
 
+# Function to initialise the database
+def init_db():
+    """Function that create the database tables and populate with a set of default themes """
+
+    # Connect to database and create cursor
+    conn, cursor = connect_database()
 
     # CREATE TABLES
     # -- x----------------x --
@@ -39,9 +58,9 @@ def init_db():
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS cards (
             id INT PRIMAARY KEY
-            , reponse TEXT 
-            , probability REAL
-            , id_theme INT 
+            , response TEXT STRICT
+            , probability REAL STRICT
+            , id_theme INT STRICT
             , FOREIGN KEY (id_theme) REFERENCES themes(id) ON DELETE RESTRICT
         );
     ''')
@@ -50,16 +69,16 @@ def init_db():
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS themes (
             id INT PRIMARY KEY
-            , theme TEXT NOT NULL
+            , theme TEXT STRICT NOT NULL
         );
     ''')
 
     # Create STATS table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS stats (
-            id INT PRIMARY KEY
-            , good_answers INT
-            , wrong_answers INT
+            id INT STRICT PRIMARY KEY
+            , good_answers INT STRICT
+            , wrong_answers INT STRICT
             , date DATE
         );
     ''')
@@ -85,16 +104,8 @@ def init_db():
     # -- x----------------x --
 
 
-
     # CLOSING PROTOCOL
-    # -- x----------------x --
-    # Validate modifications
-    conn.commit()
-    # Close the connection
-    conn.close()
-    # Display a message
-    print('\nThe database connexion have been properly closed !\n')
-    # -- x----------------x --
+    close_db_connection(conn)
 
 
 # -- x-------------------------------------x --
