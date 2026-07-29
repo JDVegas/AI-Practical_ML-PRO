9d1ec872b4368ff33c269c174085e74a81388022
JDVegas <durand-vegas.johann@hotmail.fr>
2026-07-29 16:23:23 +0200

    Add function to remove cards, extract them all, get the total number of cards, extract all the cards associated to a specific them

--- 01_Pilier_de_lIA/Formation_SQL/Flashcards_Exercice/crud_functions_cards_table.py
+++ 01_Pilier_de_lIA/Formation_SQL/Flashcards_Exercice/crud_functions_cards_table.py
@@ -146,7 +146,18 @@ def delete_card(id: int):
         Args :
             id (int): the id of the card that must be removed
     """
+    # Connect to the database
+    conn, cursor = connect_database()
 
+    # Remove a card
+    cursor.execute('''
+        DELETE FROM cards WHERE id = ?;
+    ''', (id,))
+    # Display a message 
+    print(f"\nThe card {id} have been properly removed from the deck !\n")
+
+    # Database closing protocol
+    close_db_connection(conn)
 
 
 # Define a function to get all cards
@@ -157,6 +168,36 @@ def get_all_cards() -> Dict:
             cards_dict (Dict) : a dictionary containing all the cards of the deck
     """
 
+    # Connect to the database
+    conn, cursor = connect_database()
+
+    # Extract all the cards from the deck
+    cards = cursor.execute('''
+        SELECT * from cards;
+    ''')
+
+    # Fetch the selected elements from the cursor
+    cards = cursor.fetchall()
+    
+
+    # Extract all cards from the result and format them into a dictionary
+    # -- x------------------------x --
+    # Format as a dictionary of lists --> {'id': [...], 'question': [...], ...}
+    cards_dict = {col[0]:[card[idx] for card in cards] for idx, col in enumerate(cursor.description)} 
+    # --> Probably the better option as more compact
+   
+    # Format as a list of dictionaries --> [{'id':value, 'question':value, ...}, {'id':value, ...}, ...]
+    #cards_dict = list(map(lambda x: {cursor.description[idx][0]: col for idx, col in enumerate(x)}, cards)) # 
+    
+    #print('\n', cards_dict)
+    # -- x------------------------x --
+
+    # Database closing protocol
+    close_db_connection(conn)
+
+    return cards_dict
+
+
 
 # Define a function to get the number of cards 
 def get_number_of_cards() -> int:
@@ -166,15 +207,52 @@ def get_number_of_cards() -> int:
             total_number (int) : The total number of cards of the deck 
     """
 
+    # Connect to the database
+    conn, cursor = connect_database()
+
+    # Get the number of cards of the deck
+    total_number = cursor.execute('''
+    SELECT COUNT(*) FROM cards; 
+    ''')
+
+    # Extract the info from the cursor result
+    total_number = total_number.fetchone()[0]
+
+    # Database closing protocol
+    close_db_connection(conn)
+
+    return total_number
+
 
 # Define a function that returns the card according to a selected theme
 def get_cards_by_theme(id_theme: int) -> Dict:
-    """
+    """ Extract all the cards for a specific theme
+
         Args : 
+            id_theme (int) : the unique identifier of a theme
 
         Returns : 
+            theme_cards_dict (Dict) : all the cards related the indicated theme
     """
+    # Connect to the database
+    conn, cursor = connect_database()
 
+    # Extract the cards from the database
+    cards = cursor.execute('''
+    SELECT * FROM cards
+    WHERE id_theme = ?
+    ''', (id_theme,))
+
+    # Extract the data from the cursor result
+    cards = cards.fetchall()
+
+    # Format the data 
+    cards_dict = {col[0]:[card[idx] for card in cards] for idx, col in enumerate(cursor.description)} 
+
+    # Database closing protocol
+    close_db_connection(conn)
+
+    return cards_dict
 
 
 
