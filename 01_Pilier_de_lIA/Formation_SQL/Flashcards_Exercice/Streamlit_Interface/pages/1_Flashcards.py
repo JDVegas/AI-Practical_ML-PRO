70b5216a9b77868fcdc2719c3e682a9ba0175e0f
JDVegas <durand-vegas.johann@hotmail.fr>
2026-08-17 02:39:46 +0200

    Add a slider and additional function to define the number of cards to study and build the deck to study

--- 01_Pilier_de_lIA/Formation_SQL/Flashcards_Exercice/Streamlit_Interface/pages/1_Flashcards.py
+++ 01_Pilier_de_lIA/Formation_SQL/Flashcards_Exercice/Streamlit_Interface/pages/1_Flashcards.py
@@ -11,6 +11,9 @@
 # -- x-------------------------x --
 # Standard libraries
 import sys
+import math
+import random
+from itertools import islice
 from pathlib import Path
 import pandas as pd
 
@@ -71,14 +74,77 @@ def load_cards_by_theme(theme_id: int):
     # Load the cards related to this theme
     cards = cfct.get_cards_by_theme(theme_id)
     # Format theme
-    st.session_state.cards_selected_theme = [{key:cards[key][idx] for key in cards.keys()} for idx in range(len(cards['id']))]
+    st.session_state.selected_theme_cards = [{key:cards[key][idx] for key in cards.keys()} for idx in range(len(cards['id']))]
 
     # Help debugging 
-    print(f"[cards_selected_theme]:\n{st.session_state.cards_selected_theme}")
+    print(f"[selected_theme_cards]:\n{st.session_state.selected_theme_cards}")
+
+    # Update the them in the session
+    st.session_state.selected_theme = theme_index[theme_id]
 
     # Display a message to the users to indicate that the card are well loaded
-    if st.session_state.cards_selected_theme :
-        st.toast(f"The card related to the theme '{theme_index[theme_id]}' have been properly loaded. The Deck contains {len(st.session_state.cards_selected_theme)} cards.")
+    if st.session_state.selected_theme_cards :
+        st.toast(f"The card related to the theme '{theme_index[theme_id]}' have been properly loaded. The Deck contains {len(st.session_state.selected_theme_cards)} cards.")
+
+
+# Define a plumbing function to prepare the deck to stury
+def session_deck_to_study(num_cards_to_study: int):
+    """ Plumbig function to build the deck with the cards from the theme the user want to study 
+    
+        Args :
+            num_cards_to_study (int): the number of card the user want to study during a specific session
+    """
+    # IF .. the amount of card to study is changing, then update the session value
+    if num_cards_to_study != st.session_state.number_cards_to_study:
+        st.session_state.number_cards_to_study = num_cards_to_study
+
+
+    # Order the card from the `selected_theme_cards` according to their probability
+    #sorted_cards_by_prob =  sorted(st.session_state.selected_theme_cards, key=lambda item: item["probability"], reverse=True)
+    sorted_cards_by_prob =  sorted(st.session_state.cards, key=lambda item: item["probability"], reverse=True)
+
+    #print(f"\n[sorted_cards_by_prob]: {sorted_cards_by_prob}\n")
+    #print(f"\n[sorted_cards_by_prob]: {type(sorted_cards_by_prob)}\n")
+
+
+    # Define the number of cards to extract
+    # -- x------------x --
+    # 60 % must be the less known cards
+    wrongly_known_number = math.ceil(num_cards_to_study*0.6)
+    # 30 % must be well know card to study them back
+    well_known_number = math.ceil((num_cards_to_study - wrongly_known_number)*0.3)
+    # 10 % must be new news cards : 
+    new_cards_number = num_cards_to_study - wrongly_known_number - well_known_number
+
+    # Help debuggin 
+    print(f"\n[wrongly_known_number]: {wrongly_known_number} | [well_known_number]: {well_known_number}\n")
+    # -- x------------x --
+
+
+    # Extract wrongly known cards
+    wrongly_know_cards = [item for item in sorted_cards_by_prob if item["probability"] < 0.8]
+    # Extract well known cards
+    well_know_cards = [item for item in sorted_cards_by_prob if item["probability"] >= 0.8]
+    # Extract new cards
+    #new_cards = [item for item in sorted_cards_by_prob if item["new"] == True]
+
+    # Select the cards for the session 
+    # -- x------------x --
+    # 60 % must be the less known cards
+    training_session_cards = wrongly_know_cards[:wrongly_known_number]
+    #print(len(training_session_cards))
+    # 30 % must be well know card to study them back
+    training_session_cards += random.sample(
+        well_know_cards, well_known_number if well_known_number < len(well_know_cards) else len(well_know_cards)
+    )
+    #print(len(training_session_cards))
+
+    # 10 % must be new news cards : 
+    # -> Think about adding a boolean column into the card table to indicate the new cards
+    # -- x------------x --
+
+
+
 # -- x-----------------------------------------x --
 
 
@@ -96,10 +162,42 @@ with st.sidebar:
                         ,options=st.session_state.themes
                         , format_func=lambda x: x["theme"]
                     )
-
+    
+    
     # IF .. the user select a theme, then load the cards related to this thele
-    if selected_theme:
+    if selected_theme != st.session_state.selected_theme:
         load_cards_by_theme(selected_theme['id'])
+        # Help debugging
+        print(f"\n[selected_theme_cards]: {st.session_state.selected_theme_cards}\n")
+
+        
+
+    #st.write("Chopse the number of cards to study:")
+    # Create a list for sliders
+    #range_cards_to_study = list(range(5, 50, 5))
+    # Instantiate a slider to choose the number of card to study each session
+    num_cards_to_study = st.slider(
+        label="Choose the number of cards to study:"
+        , min_value=5
+        , max_value=50
+        , value=st.session_state.number_cards_to_study
+        , step=5
+    )
+
+    # IF .. the number of cards to study change OR the theme to study change, then rebuild the deck
+    if num_cards_to_study != st.session_state.number_cards_to_study \
+        or selected_theme != st.session_state.selected_theme:
+
+        # Call the function to prepare the deck to study for the session
+        session_deck_to_study(num_cards_to_study)
+
+    
+    # -- x------------x --
+
+
+
+
+
 
 
 
@@ -111,7 +209,7 @@ with st.sidebar:
 
 
 
-""'''
+
 # -- PAGE CONTAINT
 # -- x-----------------------------------------x --
 st.title("Flashcards")
@@ -119,11 +217,18 @@ st.title("Flashcards")
 # Insert Space below the title
 st.write("##")
 
+# IF .. a theme is selected, then indicat its name
+if selected_theme:
+    st.markdown(f"<b><u>THEME</u>: {selected_theme['theme']}<b>", unsafe_allow_html=True) 
+    st.markdown(f"This theme contains {len([card for card in st.session_state.cards if card['id_theme'] == selected_theme['id']])} cards")
+
+
 # Create 3 columns
 col1, col2, col3 = st.columns((1, 3, 1))
 
 # Build the column elements 
 with col2:
+
 
     # Build a container to display a card shape
     with st.container(border=True, height=300, width="stretch"
@@ -131,7 +236,7 @@ with col2:
                     , vertical_alignment="top"):
         
 
-        st.markdown(f"""<h1 style="background-color:powderblue;">My_theme</h1>""",unsafe_allow_html=True) 
+        #st.markdown(f"""<h1 style="background-color:powderblue;">My_theme</h1>""",unsafe_allow_html=True) 
         # Build a container without border to display the question
         with st.container(height="stretch", width="stretch"
                         , horizontal_alignment="center"
@@ -142,7 +247,7 @@ with col2:
             if not st.session_state.show_answer:   
                 st.markdown("<p style='text-align:center;'>My_question</p>", unsafe_allow_html=True)
             else : 
-                st.markdown("<p><span style='text-align:center;'>My_question</span></p>", unsafe_allow_html=True)
+                st.markdown("<p style='text-align:center;'>My_question</p>", unsafe_allow_html=True)
                 st.markdown("<p style='text-align:center;'>The_correct_answer</p>", unsafe_allow_html=True)
 
     st.button(label="Show answer", width="stretch", on_click=toggle_answer)
@@ -160,4 +265,4 @@ with col2:
 
     with subcol23: 
         #if 
-        st.button(label="Wrong", width="stretch")'''""
+        st.button(label="Wrong", width="stretch")
