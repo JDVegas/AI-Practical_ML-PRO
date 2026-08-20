507aebc5725ea28af962927f803c45a6ad7cbbf8
JDVegas <durand-vegas.johann@hotmail.fr>
2026-08-20 03:44:38 +0200

    Add some functio to try to manage cards learning process

--- 01_Pilier_de_lIA/Formation_SQL/Flashcards_Exercice/Streamlit_Interface/pages/1_Flashcards.py
+++ 01_Pilier_de_lIA/Formation_SQL/Flashcards_Exercice/Streamlit_Interface/pages/1_Flashcards.py
@@ -13,6 +13,7 @@
 import sys
 import math
 import random
+from typing import List, Dict
 from itertools import islice
 from pathlib import Path
 import pandas as pd
@@ -31,6 +32,7 @@ sys.path.insert(0, str(root_dir))
 from Modules.app_initialization import app_initialization
 import Modules.crud_functions_cards_table as cfct 
 import Modules.crud_functions_themes_table as cftt
+import Modules.statistics as stat
 # -- x-------------------------x --
 
 
@@ -79,7 +81,7 @@ def load_cards_by_theme(theme_id: int):
     # Help debugging 
     print(f"[selected_theme_cards]:\n{st.session_state.selected_theme_cards}")
 
-    # Update the them in the session
+    # Update the theme in the session
     st.session_state.selected_theme = theme_index[theme_id]
 
     # Display a message to the users to indicate that the card are well loaded
@@ -100,8 +102,8 @@ def session_deck_to_study(num_cards_to_study: int):
 
 
     # Order the card from the `selected_theme_cards` according to their probability
-    #sorted_cards_by_prob =  sorted(st.session_state.selected_theme_cards, key=lambda item: item["probability"], reverse=True)
-    sorted_cards_by_prob =  sorted(st.session_state.cards, key=lambda item: item["probability"], reverse=True)
+    sorted_cards_by_prob =  sorted(st.session_state.selected_theme_cards, key=lambda item: item["probability"], reverse=True)
+    #sorted_cards_by_prob =  sorted(st.session_state.cards, key=lambda item: item["probability"], reverse=True)
 
     #print(f"\n[sorted_cards_by_prob]: {sorted_cards_by_prob}\n")
     #print(f"\n[sorted_cards_by_prob]: {type(sorted_cards_by_prob)}\n")
@@ -128,24 +130,101 @@ def session_deck_to_study(num_cards_to_study: int):
     # Extract new cards
     #new_cards = [item for item in sorted_cards_by_prob if item["new"] == True]
 
-    # Select the cards for the session 
+
+
+    # Select the cards for the session   : MUST BE UPDATED AND ADJUSTED WHEN THE BOOLEAN COLUMN WILL BE CREATED WITHIN DATABASE
     # -- x------------x --
     # 60 % must be the less known cards
     training_session_cards = wrongly_know_cards[:wrongly_known_number]
     #print(len(training_session_cards))
     # 30 % must be well know card to study them back
     training_session_cards += random.sample(
-        well_know_cards, well_known_number if well_known_number < len(well_know_cards) else len(well_know_cards)
+        well_know_cards
+        , well_known_number if well_known_number < len(well_know_cards) else len(well_know_cards)
     )
-    #print(len(training_session_cards))
+    # 10 % must be new news cards : 
+    #training_session_cards += random.sample(sorted_cards_by_prob, num_cards_to_study-len(training_session_cards))
+    training_session_cards += random.sample(sorted_cards_by_prob, min(num_cards_to_study, len(training_session_cards)))
+
+    # Help debugging
+    print("\nNumber of card within the session deck: ", len(training_session_cards))
+    print("Current session deck:\n", training_session_cards)
+
 
     # 10 % must be new news cards : 
     # -> Think about adding a boolean column into the card table to indicate the new cards
     # -- x------------x --
 
+    return training_session_cards
+
+
+# Define a function to start a session
+def start_session():
+    """ Indicate that the session has started """
+    st.session_state.learning_session_running = True
+
+
+# Define a function to save user answers and return results
+def save_and_go_next(answers: str): #-> Dict[List[(bool, str)]]:
+    """  Plumbing function that allow to update the current card probability
+    
+        Args :
+            answers (str): the user answers list
+
+        Return: 
+            result (): the session results
+    """
+
+    # Iterate through each answer
+    for id, answer in answers:
+        print(f"\n[ID]: {id} | [Answer]: {answer}\n")
+
+    '''
+        # Define a match case structure
+        match answer:
+            case "correct":
+                # Update card probability
+                stat.update_card_probability(id, True)
+                # Update stats
+                stat.update_stats(True)
+            case "no_idea":
+                # Update card probability
+                stat.update_card_probability(id, False)
+                # Update stats
+                stat.update_stats(False)
+            case "wrong":
+                # Update card probability
+                stat.update_card_probability(id, False)
+                # Update stats
+                stat.update_stats(False)
+
+    # Update the selected cards in case the user would do another learning session 
+    load_cards_by_theme(selected_theme['id'])
+    '''
+
+
+    
+    
+
+# Define a function to switch card
+def next_card(answer: dict):
+    """ Plumbing function that allow to change card """
+    # Update the learning_session_answers table
+    st.session_state.learning_session_answers.append(answer)
+    print(f"\n{st.session_state.current_card} - [answer_list]: {st.session_state.learning_session_answers}\n")
+
+
+    # Increment the current_card value
+    st.session_state.current_card += 1
+    # Reset the show_answer variable
+    st.session_state.show_answer = False
+    print(f"\n[CURRENT CARD NUMBER after next_card function]: {st.session_state.current_card}\n")
 
 
 # -- x-----------------------------------------x --
+
+
+
 
 
 
@@ -189,7 +268,7 @@ with st.sidebar:
         or selected_theme != st.session_state.selected_theme:
 
         # Call the function to prepare the deck to study for the session
-        session_deck_to_study(num_cards_to_study)
+        training_session_cards = session_deck_to_study(num_cards_to_study)
 
     
     # -- x------------x --
@@ -210,59 +289,135 @@ with st.sidebar:
 
 
 
+
 # -- PAGE CONTAINT
 # -- x-----------------------------------------x --
 st.title("Flashcards")
 
 # Insert Space below the title
-st.write("##")
+#st.write("##")
+st.markdown("<br>", unsafe_allow_html=True)
 
 # IF .. a theme is selected, then indicat its name
-if selected_theme:
+if selected_theme["theme"] != "":
+    #print("THEME SELECTED")
+
+    
     st.markdown(f"<b><u>THEME</u>: {selected_theme['theme']}<b>", unsafe_allow_html=True) 
     st.markdown(f"This theme contains {len([card for card in st.session_state.cards if card['id_theme'] == selected_theme['id']])} cards")
 
+    st.write("<br>", unsafe_allow_html=True)
 
-# Create 3 columns
-col1, col2, col3 = st.columns((1, 3, 1))
+    # Create 3 columns
+    col1, col2, col3 = st.columns((1, 3, 1))
 
-# Build the column elements 
-with col2:
+    if  st.session_state.learning_session_running:
 
+        # Create 3 columns
+        #col1, col2, col3 = st.columns((1, 3, 1))
 
-    # Build a container to display a card shape
-    with st.container(border=True, height=300, width="stretch"
-                    , horizontal_alignment="center"
-                    , vertical_alignment="top"):
+        # Renitialiaze the training session 
+        #st.session_state.current_card = 0
+        # Initialize the answer list
+        #answer_list = []
         
 
-        #st.markdown(f"""<h1 style="background-color:powderblue;">My_theme</h1>""",unsafe_allow_html=True) 
-        # Build a container without border to display the question
-        with st.container(height="stretch", width="stretch"
-                        , horizontal_alignment="center"
-                        , vertical_alignment="center"):   
+        # Build the column elements 
+        with col2:
 
 
-            # IF ...  
-            if not st.session_state.show_answer:   
-                st.markdown("<p style='text-align:center;'>My_question</p>", unsafe_allow_html=True)
-            else : 
-                st.markdown("<p style='text-align:center;'>My_question</p>", unsafe_allow_html=True)
-                st.markdown("<p style='text-align:center;'>The_correct_answer</p>", unsafe_allow_html=True)
+            # Build a container to display a card shape
+            with st.container(border=True, height=300, width="stretch"
+                            , horizontal_alignment="center"
+                            , vertical_alignment="top"):
+                
 
-    st.button(label="Show answer", width="stretch", on_click=toggle_answer)
-
-    subcol21, subcol22, subcol23 = st.columns(3)
-
-    with subcol21: 
-        #if 
-        st.button(label="Correct", width="stretch")
+                #st.markdown(f"""<h1 style="background-color:powderblue;">My_theme</h1>""",unsafe_allow_html=True) 
+                # Build a container without border to display the question
+                with st.container(height="stretch", width="stretch"
+                                , horizontal_alignment="center"
+                                , vertical_alignment="center"):   
 
 
-    with subcol22: 
-        #if 
-        st.button(label="No Idea", width="stretch")
+                    # Help debugging 
+                    print(f"\n[CURRENT CARD NUMBER]: {st.session_state.current_card}\n")
 
-    with subcol23: 
-        #if 
-        st.button(label="Wrong", width="stretch")
+                    # IF .. the user did not ask to see the answer, then only display the question
+                    if not st.session_state.show_answer:   
+                        st.markdown(f"<p style='text-align:center;'>{training_session_cards[st.session_state.current_card]['question']}</p>", unsafe_allow_html=True)
+                    # ELSE .. the user asked to see the answer, thenk show the question and the answer
+                    else : 
+                        st.markdown(f"<p style='text-align:center;'>{training_session_cards[st.session_state.current_card]['question']}</p>", unsafe_allow_html=True)
+                        st.markdown(f"<p style='text-align:center;'>{training_session_cards[st.session_state.current_card]['response']}</p>", unsafe_allow_html=True)
+
+            # Define a button to let the user see the answer to check if he found the correct one
+            st.button(label="Show answer", width="stretch", on_click=toggle_answer)
+
+
+            # Define three columns to contain 3 buttons, 1 button each 
+            # The user will have to indicate of he found the correct answer in his head
+            subcol21, subcol22, subcol23 = st.columns(3)
+
+     
+
+            # Define a button s
+            with subcol21: 
+                # Define a button that will answer the specified answer
+                st.button(
+                    label="Correct"
+                    , width="stretch"
+                    , on_click=next_card
+                    , args = (
+                        {
+                            "id": training_session_cards[st.session_state.current_card]["id"]
+                            , "answer": "correct"
+                        }, 
+                    )
+                )
+       
+
+
+            with subcol22: 
+                # Define a button that will answer the specified answer
+                st.button(
+                    label="No Idea"
+                    , width="stretch"
+                    , on_click=next_card
+                    , args = (
+                        {
+                            "id": training_session_cards[st.session_state.current_card]["id"]
+                            , "answer": "no_idea"
+                        }, 
+                    )
+                )
+
+            with subcol23: 
+                # Define a button that will answer the specified answer
+                st.button(
+                    label="Wrong"
+                    , width="stretch"
+                    , on_click=next_card
+                    , args = (
+                        {
+                            "id": training_session_cards[st.session_state.current_card]["id"]
+                            , "answer": "wrong"
+                        }, 
+                    )
+                )
+
+            print(f"[answer_list]: {st.session_state.learning_session_answers}")
+
+            # Update the session learning_session_answers
+            #st.session_state.learning_session_answers = answer_list
+
+            # IF .. all the training session deck have been explored, then stop the session
+            if len(training_session_cards) == st.session_state.current_card:
+                st.session_state.learning_session_running = False
+
+
+    elif not st.session_state.learning_session_running and st.session_state.learning_session_answers:
+        print("BOB")
+
+    else:
+        with col2:
+            st.button(label="Start Session", width="stretch", on_click=start_session)
