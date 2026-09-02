4f5b31f1235280689c998307a76c1b21a2c9cb1f
JDVegas <durand-vegas.johann@hotmail.fr>
2026-09-02 15:48:53 +0200

    First FastAPI instantiation

--- 05_MLOps/FastAPI/01_Premiers_Pas/main.py
+++ 05_MLOps/FastAPI/01_Premiers_Pas/main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from 01-premiers-pas!")
 
 
-if __name__ == "__main__":
-    main()
+
+# -- SETUP -- 
+# -- x-------------------------------x --
+# Standard libraries
+
+# API libraries
+from fastapi import FastAPI
+
+
+
+# Instantiate the FastAPI APP oject
+app = FastAPI(
+	title = "First Steps With FastAPI"
+	, description = "Demo API"
+	, version = "1.0.0"
+)
+# -- x-------------------------------x --
+
+
+
+
+
+
+
+# -- SCRIPT -- 
+# -- x-------------------------------x --
+
+
+# -- x-------------------------------x --
